5195581c8b39f9b7f24a03a7c01051dfcb7491e8
Luis Vega <luiscvega@gmail.com>
2014-03-01 17:58:19 +0800

    Added md5 hash generator for gravatar

--- middleware/user/model.js
+++ middleware/user/model.js
@@ -1,8 +1,16 @@
 var Schema = require("jugglingdb").Schema;
 var schema = new Schema("redis", { port: 6379 });
+var md5 = require("crypto").createHash("md5");
 
-module.exports = schema.define("User", {
+var User = schema.define("User", {
   username: { type: String, index: true },
   email: { type: String, index: true },
   cryptedPassword: String
 });
+
+User.prototype.gravatar = function () {
+  var gravatar = "https://secure.gravatar.com/avatar/" + md5.update(this.email).digest("hex") + ".jpg";
+  return gravatar;
+};
+
+module.exports = User;
